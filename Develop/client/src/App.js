6c15e3dfacf82051030c2d3d71bdc343d768c0de
Richmond Dominguez <richmonddominguez@gmail.com>
2022-08-23 22:14:12 -0700

    added apollo client to app.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,6 +4,13 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
 function App() {
   return (
     <Router>
