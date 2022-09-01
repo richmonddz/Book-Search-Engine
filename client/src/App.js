e95b85020437b4019b5b1b0a60367365698ef38f
Richmond Dominguez <richmonddominguez@gmail.com>
2022-08-31 22:42:08 -0700

    diagnosing errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
