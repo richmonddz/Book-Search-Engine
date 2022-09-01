5a2538ad42ffe30323546372fb159ed421a84c33
Richmond Dominguez <richmonddominguez@gmail.com>
2022-08-31 21:27:22 -0700

    cleaned up files, got rid of non used files, installed packages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+
 const path = require("path");
 const db = require("./config/connection");
 //const routes = require("./routes");
