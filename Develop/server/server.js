2b519bd9e3619c1ab21534186886e4c60d56b30f
Richmond Dominguez <richmonddominguez@gmail.com>
2022-08-31 19:06:11 -0700

    updated sever and applied as middle ware to expres server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const routes = require("./routes");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const path = require("path");
+const db = require("./config/connection");
+//const routes = require("./routes");
 
 const { typeDefs, resolvers } = require("./schemas");
 
@@ -26,8 +26,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+//app.use(routes);
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
 });
